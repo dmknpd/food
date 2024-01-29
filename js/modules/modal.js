94e9918d712b323a669f3c047c292f5ae18793fc
dmknpd <soupuabt@gmail.com>
2024-01-29 13:46:54 +0200

    add modules

---
+++ js/modules/modal.js
@@ -0,0 +1,51 @@
+function modal() {
+  //MODAL
+
+  const modal = document.querySelector(".modal");
+  const btnModal = document.querySelectorAll("[data-modal]");
+
+  function openModal() {
+    modal.classList.add("show");
+    modal.classList.remove("hide");
+    document.body.style.overflow = "hidden";
+    clearInterval(modelTimerId);
+  }
+
+  function closeModal() {
+    modal.classList.remove("show");
+    modal.classList.add("hide");
+    document.body.style.overflow = "";
+  }
+
+  btnModal.forEach((item) => {
+    item.addEventListener("click", openModal);
+  });
+
+  modal.addEventListener("click", (e) => {
+    if (e.target === modal || e.target.getAttribute("data-close") == "") {
+      closeModal();
+    }
+  });
+
+  document.addEventListener("keydown", (e) => {
+    if (e.code === "Escape" && modal.classList.contains("show")) {
+      closeModal();
+    }
+  });
+
+  const modelTimerId = setTimeout(openModal, 50000);
+
+  function showModalByScroll() {
+    if (
+      window.scrollY + document.documentElement.clientHeight >=
+      document.documentElement.scrollHeight
+    ) {
+      openModal();
+      window.removeEventListener("scroll", showModalByScroll);
+    }
+  }
+
+  window.addEventListener("scroll", showModalByScroll);
+}
+
+module.exports = modal;
